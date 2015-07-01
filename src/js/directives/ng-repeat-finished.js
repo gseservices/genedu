2ba8c08f8f76fc6b4786966fc8aa4100a5abe365
dipesh pachlegaonkar <dipesh.pachlegaonkar@gmail.com>
2015-07-01 17:42:54 +0530

    changes to clsreceipt

---
+++ src/js/directives/ng-repeat-finished.js
@@ -0,0 +1,10 @@
+angular.module('app', [])
+.directive('ngRepeatFinished', function () {
+    return function(scope, element, attrs) {
+    if (scope.$last){
+      // iteration is complete, do whatever post-processing
+      // is necessary
+      element.parent().css('border', '1px solid black');
+    }
+  };
+});
